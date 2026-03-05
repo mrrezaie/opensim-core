cbcfba411aedaffc6379ed7c5eebba59676b725b
Mohammadreza Rezaie <mrrezaie@outlook.com>
2026-03-05 13:45:20 +0100

    Update __init__.py

--- Bindings/Python/__init__.py
+++ Bindings/Python/__init__.py
@@ -2,19 +2,16 @@ import sys
 import os
 
 curFolder = os.path.dirname(os.path.realpath(__file__))
-# if (sys.platform.startswith('win')):
-#     os.add_dll_directory(curFolder)
-#     # When installed locally via "python -m pip install ." in Windows
-#     if os.path.isfile(os.path.join(curFolder, 'opensim-cmd.exe')):
-#         # This only sets the PATH for the Python environment (not permanent)
-#         os.environ['PATH'] = curFolder + os.pathsep + os.environ['PATH']
-#     # For local testing
-#     install_path = os.path.join(curFolder, "../../../bin")
-#     if (os.path.exists(install_path)):
-#         os.add_dll_directory(install_path)
-#     dev_path = os.path.join(curFolder, "../../../../Release")
-#     if (os.path.exists(dev_path)):
-#         os.add_dll_directory(dev_path)
+if (sys.platform.startswith('win')):
+    os.add_dll_directory(curFolder)
+    os.environ['PATH'] = curFolder + os.pathsep + os.environ['PATH']
+    # For local testing
+    install_path = os.path.join(curFolder, "../../../bin")
+    if (os.path.exists(install_path)):
+        os.add_dll_directory(install_path)
+    dev_path = os.path.join(curFolder, "../../../../Release")
+    if (os.path.exists(dev_path)):
+        os.add_dll_directory(dev_path)
 
 from .simbody import *
 from .common import *
