90b4408576d869f536f6c1f6bbb8f31b78d3b157
Mohammadreza Rezaie <mrrezaie@outlook.com>
2026-08-16 15:11:40 +0200

    Revert "include muscle physiology"
    This reverts commit 256ec52deff55af2471d6c92f653222d85470624.

--- OpenSim/JAM/COMAKTarget.h
+++ OpenSim/JAM/COMAKTarget.h
@@ -131,8 +131,12 @@ public:
          _optimal_force = optimal_force;
     }
 
-    void setPassiveForces(SimTK::Vector passive_force) {
-         _passive_force = passive_force;
+    void setActiveElementForces(SimTK::Vector active_element_force) {
+         _active_element_force = active_element_force;
+    }
+
+    void setPassiveElementForces(SimTK::Vector passive_element_force) {
+         _passive_element_force = passive_element_force;
     }
 
     void setMuscleVolumes(SimTK::Vector msl_volumes) {
@@ -166,7 +170,8 @@ private:
     Model *_model;
     SimTK::State _state;
     SimTK::Vector _optimal_force;
-    SimTK::Vector _passive_force;    
+    SimTK::Vector _active_element_force;
+    SimTK::Vector _passive_element_force;    
     const SimTK::Vector _init_parameters;
     double _activation_exponent;
     SimTK::Vector _observed_udot;
